540987198f3e7d4a32bf82508e4e83e58dc595e9
Tiyo <vmkvo15@outlook.com>
2023-08-30 18:46:28 +0400

    Update avatar container class retrieval name

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '../src/style.css'
 import avatar from './assets/avatar.jpg';
 
-const avatarContainer = document.querySelector('.avatarContainer');
+const avatarContainer = document.querySelector('.a-container');
 avatarContainer.src = avatar;
