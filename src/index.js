28af67393f8feeae53fbdce086d2957a18988600
Tiyo <vmkvo15@outlook.com>
2023-09-01 15:24:58 +0400

    Add scroll img arrow & field

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import '../src/style.css'
 import avatar from './assets/avatar.jpg';
+import arrowDown from './assets/scrollDown.png';
 
-const avatarContainer = document.querySelector('.a-container');
-avatarContainer.src = avatar;
+const avatarImg = document.querySelector('.a-img');
+const scrollImg = document.querySelector('.s-img');
+avatarImg.src = avatar;
+scrollImg.src = arrowDown
+
+document.body.style.display = 'flex'
